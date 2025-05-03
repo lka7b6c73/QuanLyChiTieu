627d59a46029db1c240e40d2eeb1b382956368f0
lka7b6c73 <86911001+lka7b6c73@users.noreply.github.com>
2025-05-03 23:53:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ if st.button("Xác nhận"):
   # yyyy-mm-dd
 
         # Ghi lên Google Sheets: Tháng, Ngày, Thời gian, Mô tả, Loại, Giá, Ngày đầy đủ
-        worksheet.append_row([thang, ngay, thoigian, mota, boxi, gia, '', ngay_daydu])
+        worksheet.append_row([thang, ngay, thoigian, mota, boxi, gia, ngay_daydu])
         st.success(f"Đã ghi: {thang}/{ngay} {thoigian} - {mota} - {boxi} - {gia} nghìn đồng (Ngày đầy đủ: {ngay_daydu})")
     else:
         st.error("Vui lòng nhập đầy đủ thông tin.")
